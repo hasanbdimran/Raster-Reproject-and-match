0c5e372f26f16f1c0b0861b094816483dc9a94dd
Mohammad Imran Hasan <hasanbdimran@gmail.com>
2023-06-26 12:20:50 -0500

    Create Pythonscript.py

---
+++ Pythonscript.py
@@ -0,0 +1,9 @@
+import rioxarray # for the extension to load
+import xarray
+## Loading two raster for reproject and match operation
+xds = xarray.open_dataarray(os.path.join(file_dir, 'rasterize', 'test.tif'))
+xds_match = xarray.open_dataarray(r"/Users/ihasan/Downloads/TAMU/TTI_project/dem_30m_mosaic/TX_NADEP_30m.tif")
+
+xds_repr_match = xds.rio.reproject_match(xds_match, nodata=-1)
+
+xds_repr_match.rio.to_raster(os.path.join(file_dir, 'rasterize', 'test1.tif'))
